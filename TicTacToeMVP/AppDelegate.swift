108048e173ee64ce5ffec51161242c66540a3914
animevn <haanhth21@gmail.com>
2020-02-05 09:35:45 +0700

    prepare

--- TicTacToeMVP/AppDelegate.swift
+++ TicTacToeMVP/AppDelegate.swift
@@ -1,36 +1,36 @@
-//
-//  AppDelegate.swift
-//  TicTacToeMVP
-//
-//  Created by Anh Ha on 2/5/20.
-//  Copyright © 2020 Anh Ha. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)->Bool{
         return true
     }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+              print("app did become active")
+       }
+       
+       func applicationWillResignActive(_ application: UIApplication) {
+          print("app will become inactive")
+       }
+       
+       func applicationDidEnterBackground(_ application: UIApplication) {
+           print("app did enter background")
+       }
 
-    // MARK: UISceneSession Lifecycle
+       func applicationWillEnterForeground(_ application: UIApplication) {
+          print("app will become active")
+       }
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+       func applicationWillTerminate(_ application: UIApplication) {
+           print("app did terminate")
+       }
+          
 
 
 }
